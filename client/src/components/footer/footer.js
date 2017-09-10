791df678ab49502b82be2579bff24c61b600406f
Margaret Choi <margaret.j.li@gmail.com>
2017-09-09 17:34:45 -0700

    Initial structure of nav and footer, Add and setup Bulma/Bloomer

---
+++ client/src/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+
+
+
+    render() { 
+		return (
+			
+		<h1>Footer</h1>
+		)
+    }
+
+}
+
+export default Footer;
---
+++ client/src/components/nav/nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Nav.css";
+
+class Nav extends Component {
+
+
+
+    render() { 
+		return (
+			
+		<h1>Nav</h1>
+		)
+    }
+
+}
+
+export default Nav;
