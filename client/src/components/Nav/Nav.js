c0fee69151dc303f0685d9f1d60f18fa26c92340
leenbean16 <shamoonlina@gmail.com>
2017-09-19 18:38:30 -0700

    gonna add routes

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,6 +3,7 @@ import { Navbar, NavbarBrand, NavbarItem, NavbarLink, NavbarStart, NavbarMenu,
 	NavbarDropdown, Icon, NavbarBurger, NavbarDivider, NavbarEnd, Field,
 	Control, Button } from "bloomer";
 import logo from './parkifyLogo.jpg';
+import { NavLink } from 'react-router-dom'
 import "./Nav.css";
 
 
@@ -71,3 +72,32 @@ class Nav extends Component {
 }
 
 export default Nav;
+
+
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const Navpills = () =>
+//   <ul className="nav nav-tabs">
+//     <li className={window.location.pathname === "/" ? "active" : ""}>
+//       <Link to="/">Home</Link>
+//     </li>
+//     <li className={window.location.pathname === "/about" ? "active" : ""}>
+//       <Link to="/about">About</Link>
+//     </li>
+//     <li className={window.location.pathname === "/blog" ? "active" : ""}>
+//       <Link to="/blog">Blog</Link>
+//     </li>
+//     <li className={window.location.pathname === "/contact" ? "active" : ""}>
+//       <Link to="/contact">Contact</Link>
+//     </li>
+//     <li
+//       className={window.location.pathname === "/contact/learn" ? "active" : ""}
+//     >
+//       <Link to="/contact/learn">Learn</Link>
+//     </li>
+//   </ul>;
+
+// export default Navpills;
