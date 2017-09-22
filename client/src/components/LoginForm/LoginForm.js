d6d27b8f18870ab9134daff3f8b7a27753c7748c
Leeland <leelandmiller@gmail.com>
2017-09-22 13:12:13 -0700

    login/signup onChange events

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -3,56 +3,70 @@ import { Icon, Field, Label, Control, Input, Button } from "bloomer";
 import "./LoginForm.css";
 
 class LoginForm extends Component {
-    constructor() {
-      super();
-      this.state = {
-        isActive: false
-      };
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            isActive: false,
+            email: '',
+            password: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
-render() {
-    return (
-        <div className="form">
-            <Field>
-                <Label>
-                    Username
-            </Label>
-            <Control hasIcons>
-                <Input isColor='success' placeholder='username' value="" />
-                    <Icon isSize='small' isAlign='left'>
-                        <span className="fa fa-user" aria-hidden="true" />
-                    </Icon>
-                <Icon isSize='small' isAlign='right'>
-                    <span className="fa fa-check" aria-hidden="true" />
-                </Icon>
-            </Control>
-            </Field>
+    handleChange(event) {
 
-            <Field>
-                <Label>
-                    Password
-                </Label>
-            <Control hasIcons>
-                <Input isColor='success' placeholder='Password' value="" />
-                    <Icon isSize='small' isAlign='left'>
-                        <span className="fa fa-user-secret" aria-hidden="true" />
-                    </Icon>
-                <Icon isSize='small' isAlign='right'>
-                    <span className="fa fa-check" aria-hidden="true" />
-                </Icon>
-            </Control>
-            </Field>
+        const val = event.target.value;
+        const name = event.target.name;
 
-            <Field isGrouped>
-                <Control>
-                    <Button isColor='primary'>Submit</Button>
+        this.setState({
+            [name]: val
+        });
+    }
+
+    render() {
+        return (
+            <form className="form">
+                <Field>
+                    <Label>
+                        Email
+                    </Label>
+                    <Control hasIcons>
+                        <Input type="email" placeholder='Email' name='email' value={this.state.email} onChange={this.handleChange} />
+                        <Icon isSize='small' isAlign='left'>
+                            <span className="fa fa-user" aria-hidden="true" />
+                        </Icon>
+                        <Icon isSize='small' isAlign='right'>
+                            <span className="fa fa-check" aria-hidden="true" />
+                        </Icon>
+                    </Control>
+                </Field>
+
+                <Field>
+                    <Label>
+                        Password
+                    </Label>
+                <Control hasIcons>
+                    <Input type='password' name='password' isColor='success' placeholder='Password' value={this.state.password} onChange={this.handleChange} />
+                        <Icon isSize='small' isAlign='left'>
+                            <span className="fa fa-user-secret" aria-hidden="true" />
+                        </Icon>
+                    <Icon isSize='small' isAlign='right'>
+                        <span className="fa fa-check" aria-hidden="true" />
+                    </Icon>
                 </Control>
-                <Control>
-                    <Button isLink>Cancel</Button>
-                </Control>
-            </Field>
-            </div>
+                </Field>
+
+                <Field isGrouped>
+                    <Control>
+                        <Button isColor='primary'>Submit</Button>
+                    </Control>
+                    <Control>
+                        <Button isLink>Cancel</Button>
+                    </Control>
+                </Field>
+            </form>
         )
     }
 }
