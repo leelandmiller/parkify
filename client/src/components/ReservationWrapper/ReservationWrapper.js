aa19d6a978fd6c39a2e1c2c9a4a706bb5aa93b4e
leenbean16 <shamoonlina@gmail.com>
2017-09-23 01:16:58 -0700

    pulling

---
+++ client/src/components/ReservationWrapper/ReservationWrapper.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./ReservationWrapper.css";
+
+
+
+class FormWrapper extends Component {
+	constructor(){
+		super();
+		
+		this.state = {
+			activeLoginFormTab:"login",
+			
+		};
+		
+		this.changeActiveTab = this.changeActiveTab.bind(this);
+	}
+	
+	changeActiveTab(tab) {
+		this.setState(
+			{ activeLoginFormTab:tab }
+		)
+	};
+	
+	
+	
+	render(){
+		return(
+			<div className="formwrapper">
+				<LoginTabs
+					activeTab = { this.state.activeLoginFormTab }
+					changeActiveTab = { this.changeActiveTab } />
+				{ this.state.activeLoginFormTab === "login" ? <LoginForm /> : <SignUpForm /> }
+			</div>
+		)
+	}
+	
+}
+
+export default FormWrapper;
