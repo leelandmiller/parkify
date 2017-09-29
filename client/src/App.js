7bd644c29b8c884543be722263509b2232de84e1
hockey4life63 <Amelancon68@gmail.com>
2017-09-29 14:39:37 -0700

    Building for production

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import HomeContainer from './components/HomeContainer';
 import SimpleSearch from './components/SimpleSearch';
 import Account from './components/Account';
 import ReservationWrapper from "./components/ReservationWrapper";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AddVehicle from './components/AddVehicle';
 import API from './utils/API';
 import './App.css';
@@ -46,16 +46,16 @@ class App extends Component {
         return (
             <div id='app-container'>
                 <Nav isLoggedIn={this.state.isLoggedIn} />
-                <Router>
-                    <div>
+                <BrowserRouter>
+                    <Switch>
                         <Route exact path="/" component={HomeContainer}/>
-                        <Route exact path="/login" render={() => <FormWrapper setCurrentUser={this.setCurrentUser}/>}/>
-                        <Route exact path="/account" render={() => <Account isLoggedIn={this.state.isLoggedIn} currentUser={this.state.currentUser}/>}/>
-                        <Route exact path="/search" component={SimpleSearch}/>
-                        <Route exact path="/reserve" component={ReservationWrapper}/>
-                        <Route exact path="/add/vehicle" component={AddVehicle}/>
-                    </div>
-                </Router>
+                        <Route path="/login" render={() => <FormWrapper setCurrentUser={this.setCurrentUser}/>}/>
+                        <Route path="/account" render={() => <Account isLoggedIn={this.state.isLoggedIn} currentUser={this.state.currentUser}/>}/>
+                        <Route path="/search" component={SimpleSearch}/>
+                        <Route path="/reserve" component={ReservationWrapper}/>
+                        <Route path="/add/vehicle" component={AddVehicle}/>
+                    </Switch>
+                </BrowserRouter>
                 <PageFooter />
             </div>
         )
