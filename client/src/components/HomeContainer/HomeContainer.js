6d29c2c2b95a9142f80544b3155794ff5ff9610b
Margaret Choi <margaret.j.li@gmail.com>
2017-09-29 22:09:31 -0700

    Fix google maps results, update Nav stayling

--- client/src/components/HomeContainer/HomeContainer.js
+++ client/src/components/HomeContainer/HomeContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Title, Subtitle, Button, Column, Columns, Notification} from "bloomer";
+import { Container, Title, Subtitle, Button, Column, Columns, Notification, Icon} from "bloomer";
 import "./HomeContainer.css";
 
 const HomeContainer = props => {
@@ -9,7 +9,7 @@ const HomeContainer = props => {
 				<div className="hero">
 					<img 
 						alt="parkify hero" 
-						src="assets/images/parkify-hero.jpeg"
+						src="assets/images/hero-1.jpg"
 						className="hero-img"
 					></img>
 					<div id="hero-text">
@@ -21,26 +21,48 @@ const HomeContainer = props => {
 				<div id="home-intro">
 					<Title isSize={2}>Sell your parking spot</Title>
 					<p>Make extra moola with your parking spot.</p>
-					<a className="" href="/login">Start Now</a>
+					<a className="button is-primary" href="/login">Start Now</a>
 				</div>
-				<div id="home-select">
+			</Container>
+			<div id="home-select">
+				<Container>
 					<Title isSize={2}>Sell your spot in a snap!</Title>
 					<Columns isCentered>
 					    <Column isSize='1/3'>
-					    	<div><span className="round-number">1</span></div>
+					    	<div className="round-number">
+						    	<span>
+						    		<Icon isSize='large' icon='pencil-square-o' />
+						    	</span>
+					    	</div>
 					    	<Title isSize={5}  hasTextAlign='centered'>1. Fill out the form </Title>
+					    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar 
+					    	nibh quis ex vehicula vulputate. Nullam suscipit vehicula sapien ut dapibus.</p>
 					    </Column>
 					   	<Column isSize='1/3'>
-					   		<span className="round-number">2</span>
+						   	<div className="round-number">
+						   		<span className="round-number">
+						   			<Icon isSize='large' icon='car' />
+						   		</span>
+						   	</div>
 					   		<Title isSize={5}  hasTextAlign='centered'>2. Start renting </Title>
+					   		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar 
+					    	nibh quis ex vehicula vulputate. Nullam suscipit vehicula sapien ut dapibus.</p>
+					    
 					    </Column>
 					   	<Column isSize='1/3'>
-					   		<span className="round-number">3</span>
+						   	<div className="round-number">
+						   		<span className="round-number">
+						   			<Icon isSize='large' icon='money' />
+						   		</span>
+						   	</div>
 					        <Title isSize={5}  hasTextAlign='centered'>3. Get Paid</Title>
+					        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar 
+					    	nibh quis ex vehicula vulputate. Nullam suscipit vehicula sapien ut dapibus.</p>
+					    
 					    </Column>
 					</Columns>
-				</div>
-			</Container>
+				</Container>
+			</div>
 		</div>
 
 	)
