4f379af87b2164f15f975d6c8afc78dbfe3828e7
Leeland <leelandmiller@gmail.com>
2017-09-30 05:37:57 -0700

    fix merge conflict in Account.js

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -28,12 +28,8 @@ class Account extends Component {
 				<AccountTabs
 				activeSettingsTab = { this.state.activeSettingsTab }
 				changeActiveAccountTab = { this.changeActiveAccountTab } />
-<<<<<<< HEAD
-			{ this.state.activeSettingsTab === "AccountSettings" ? <AccountSettings {...this.props}/> : <Reservations {...this.props}/> }
 
-=======
 			{ this.state.activeSettingsTab === "AccountSettings" ? <AccountSettings {...this.props}/> : <Reservations {...this.props} /> }
->>>>>>> 3f38222bcdcf049321818ef14072ba0d0257a8b4
 			</div>
 			)
 		}
